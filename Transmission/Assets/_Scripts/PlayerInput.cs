c6553b5f0dba469c9e40ae3c74bc2d679803bb83
maxiaoxiao1991 <maxiaoxiao1991@gmail.com>
2018-01-26 21:29:03 +0200

    simple FPS player camera and movement

---
+++ Transmission/Assets/_Scripts/PlayerInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerInput : MonoBehaviour {
+
+    protected CharacterController characterController;
+
+    public string horizontalAxis;
+    public string verticalAxis;
+
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        characterController.SimpleMove(new Vector3(Input.GetAxisRaw(horizontalAxis), 0, Input.GetAxisRaw(verticalAxis)));
+
+    }
+}
