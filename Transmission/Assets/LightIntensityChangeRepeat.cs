a98b4ece8a217fb64e0d496f2a082f498e66939d
maxiaoxiao1991 <maxiaoxiao1991@gmail.com>
2018-01-27 19:26:35 +0200

    light intensity changing script

---
+++ Transmission/Assets/LightIntensityChangeRepeat.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Light))]
+public class LightIntensityChangeRepeat : MonoBehaviour {
+
+
+    public AnimationCurve changeCurve;
+    public float cyclingTime;
+    protected Light light;
+    protected float currentTime = 0;
+    private void Awake()
+    {
+        light = GetComponent<Light>();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentTime += Time.deltaTime;
+        if(currentTime >= cyclingTime)
+        {
+            currentTime = currentTime - cyclingTime;
+        }
+        light.intensity = changeCurve.Evaluate(Mathf.Clamp01(currentTime / cyclingTime));
+    }
+
+
+
+}
